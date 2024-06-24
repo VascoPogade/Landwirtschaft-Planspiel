ea5d39469d3a27709cc391d17afc8d496fd826fa
pajolinho <pgrussin@student.42berlin.de>
2024-06-24 22:56:01 +0200

    Improve Questions asked, and implement EndGame szenarios

--- src/GameController.java
+++ src/GameController.java
@@ -1,5 +1,5 @@
 public class GameController {
-    private Player player;
+private static Player player;
     private MainFrame mainFrame;
     int scenarioNumber;
 
@@ -9,40 +9,40 @@ public class GameController {
     int[][] attributes1Change;
     int[][] attributes2Change;
 
-    String questionText1 = "Bestandsaufnahme nach Vererbung. Es fällt auf es müssen dringend neue Investitionen her. Wohin soll investiert werden?";
-    String questionText2 = "Meilensteinentsheidung. Tierethik oder das große Geld?";
-    String questionText3 = "Die Hauptanbau-Pflanze auf der Farm kommt als gentechnischveränderte Pflanze auf den Markt. Allerdings is diese gentechnisch verändert. Soll das bisherige Saatgut durch das neue Veränderte ersetzt werden? ";
+    String questionText1 = "Du weißt, dass du bald die Farm deines Onkelsübernehmen wirst. Was willst du studieren um dich optimal auf das Leben eines Farmers vorzubereiten?";
+    String questionText2 = "Die Farm ist nun dein. Doch du stehst vor schwerwiegenden Entscheidungen. Möchstest du die Farm in Richtung Bio-Landwirtschaft betreiben oder entscheidest dui dich für Massentierhaltung?";
+    String questionText3 = "Die Hauptanbau-Pflanze deiner Farm kommt als gentechnischveränderte Pflanze auf den Markt. Diese sei resestenter gegenüber Krankheiten, allerdings sind deine Kunden und Arbeitnehmer skeptisch ";
     String questionText4 = "Willst du mit einer großen Supermarktkette kollaborieren, um deine Produkte dort zu verkaufen, oder eine Direct-To-Consumer Online-Plattform aufbauen?";
     String questionText5 = "Wollen sie einen Teil ihrer Anbaufläche in eine Photovoltaik Anlage verwandeln, um ihren eigenen Energiebedarf zu decken?\n\nIn Zeiten steigender Energiepreise und wachsender Besorgnis über den Klimawandel hast du die Möglichkeit, einen Teil deiner Anbaufläche in eine Photovoltaik-Anlage umzuwandeln. Dies könnte deinen eigenen Energiebedarf decken und deine Betriebskosten langfristig senken. Allerdings würdest du dadurch wertvolle Anbaufläche verlieren und hohe initiale Investitionskosten tragen müssen. Willst du einen Teil deiner Anbaufläche in eine Photovoltaik-Anlage verwandeln, um deinen eigenen Energiebedarf zu decken und gleichzeitig einen Beitrag zur Nachhaltigkeit zu leisten, auch wenn dies mit erheblichen Umstellungen und Risiken verbunden ist?";
-    String questionText6 = "Pläne für die Expansion eines großen Agragbetriebs gelangen an die Öffentlichkeit. Genau in die Nähe der Farm; Wie reagiert man auf die drohende Konkurenz?";
-    String questionText7 = "Die Arbeiter beschweren sich. Es gab zu viele Arbeitsunfälle in der Vergangenheit. Sollen die Sicherheitsregularien erhöht werden? ";
-    String questionText8 = "Wollen Sie einen neuen autonomen Traktor anschaffen?";
-    String questionText9 = "Deine Felder sind am wachsen und gedeihen. Es wir Zeit für neue Ausrüstung um diese Felder optimal zu bewirtschaften ";
-    String questionText10 = "Auf einer Fortbildung hast du über eine neue Anbaumethode dich informiert. Diese würde aber eine Fortbildung der Arbeiter bedeuten. Diene Arbeitnehmer sind gespalten; frei nach dem Mottos “Schuster bleib bei deinem Leisten” oder “Wer immer nur im alten Buch blättert, findet keine neuen Geschichten” Die Zeit ist reif, die Effizienz auf der Farm muss steigen. Wie soll dies bewerkstelligt werden?";
-    String questionText11 = "Die Hauptsaison deines Hauptanbaugemüses steht an. Es kann mehr geerntet werden.";
-    String questionText12 = " Die Umweltbehörden warnen vor der rasanten Ausbreitung einer neuen Pflanzenkrankheit.";
+    String questionText6 = "Pläne für die Expansion eines großen Agragbetriebs gelangen an die Öffentlichkeit. Die Farm liegt in der unmittelbaren Umgebung dieses neuen Großbetriebes. Wie reagieren SIe auf die drohende Konkurenz?";
+    String questionText7 = "Die Arbeiter beschweren sich. Es gab zu viele Arbeitsunfälle in der Vergangenheit. Wie wollen Sie darauf reagieren? ";
+    String questionText8 = "Wollen Sie einen neuen autonomen Traktor anschaffen?"; // TODO: Platzhalter am Dienstag ersetzen in gespräch mit anderen
+    String questionText9 = "Deine Felder gedeihen prächtig, Sie merken es wird Zeit für neue Ausrüstung, um sie optimal zu bewirtschaften.Es wir Zeit für neue Ausrüstung um diese Felder optimal zu bewirtschaften.";
+    String questionText10 = "Ein motivierter Arbeiter hat auf einer Fortbildung von einer neuen innovativen Anbaumethode gehört. Diese würde aber eine Fortbildung der Arbeiter bedeuten. Diene Arbeitnehmer sind gespalten: Einige wollen bei den alten Methoden bleiben, während andere offen für neues sind. Wie entscheiden Sie?";
+    String questionText11 = "Die Hauptsaison deines Hauptanbaugemüses steht an. Es kann mehr geerntet werden. Welche Maßnahmen ergreifen Sie um die Ernte zu maximieren und die Qualität der Produkte zu sichern?";
+    String questionText12 = " Die Umweltbehörden warnen vor der rasanten Ausbreitung einer neuen Pflanzenkrankheit. Erste berichte kommen allerdings aus dem Süden Deutschlands. Denken Sie die Krankheit breitet sich rasant aus?";
     String questionText13 = "Angesichts einer starken Inflation stehen viele deiner Arbeiter vor erheblichen finanziellen Herausforderungen. Eine Lohnerhöhung könnte ihre Kaufkraft erhalten und die Moral verbessern, würde jedoch deine Betriebskosten erheblich erhöhen. Andererseits könnte das Verwehren einer Lohnerhöhung dazu führen, dass deine Arbeiter unzufrieden und weniger produktiv werden, was die Effizienz und das Betriebsklima beeinträchtigen könnte. Willst du die Löhne deiner Arbeiter erhöhen, um ihre finanzielle Situation zu verbessern und ihre Zufriedenheit zu sichern, auch wenn dies zu höheren Fixkosten führt, oder entscheidest du dich gegen eine Lohnerhöhung, um die Kosten niedrig zu halten und dadurch möglicherweise die Produktivität und das Ansehen deines Betriebs zu riskieren?";
     String questionText14 = "Dein Nachbar hat dir anvertraut, dass er über Jahre hinweg Schulden angehäuft hat. Er hat dich gebeten, einen Teil seines Landes abzukaufen, um seine Schulden zu tilgen. Dies könnte eine Gelegenheit für dich sein, deine Farm zu erweitern, erfordert jedoch die Aufnahme eines großen Kredits, was mit erheblichen finanziellen Verpflichtungen und Risiken verbunden ist. Willst du einen großen Kredit aufnehmen, um das Land deines Nachbarn zu erwerben und ihn in seiner Notlage zu unterstützen, oder entscheidest du dich dagegen, um die finanzielle Stabilität deiner Farm zu bewahren, selbst wenn dies dein Ansehen in der Gemeinschaft beeinträchtigen könnte? ";
-    String questionText15 = "Sommer in Deutschland. Wetterexperten warnen; es stehe wieder eien trockene Jahreszeit an. Wie handeln? ";
+    String questionText15 = "Sommer in Deutschland. Wetterexperten warnen; es stehe wieder eien trockene Jahreszeit an. Wie handeln Sie? ";
     String questionText16 = " Implementierung eines IoT-Systems zur Überwachung von Umweltbedingungen \n" + //
                 "Der Farmer plant, ein Internet of Things (IoT)-System zu installieren, um Echtzeitdaten über Bodenfeuchtigkeit, Wetter und Pflanzenwachstum zu erhalten.";
     String questionText17 = "Die Regierung will eine zentrale Landstraße über ihren Acker bauen. Wollen Sie diese Entscheidung juristisch bekämpfen?\n\nIn deiner Nähe plant ein großes Industrieunternehmen eine Fabrik. Um diese Fabrik zu fördern, plant die Regierung den Bau einer Bundestraße über deinen Acker. Willst du diese Entscheidung juristisch bekämpfen?";
 
-
-    String question1Option1Text = "Investitionen in energiesparende Technologien";
-    String question1Option2Text = "Optimierung von Anbau- und Bewirtschaftungsprozessen";
-    String question2Option1Text = "Beginn der Massentierhaltung";
-    String question2Option2Text = "Ausbau der Grünflachen um den Tieren mehr Auslauf zu geben";
-    String question3Option1Text = "Komplette Saatgut austauschen ";
+    // TODO: change attributes for boost
+    String question1Option1Text = "Studium in die Richtung Energiesparende Technologien";
+    String question1Option2Text = "Im Bereich Optimierung des Anbausstudieren";
+    String question2Option1Text = "Dein Fleisch soll Preiswert sein. Du entscheidest dich für Massentierhaltung";
+    String question2Option2Text = "Auf kosten deiner Ernte baust du Felder um um darauf deinen Tieren mehr auslauf zu geben";
+    String question3Option1Text = "Komplette Saatgut austauschen und auf die neue Pflanze setzen";
     String question3Option2Text = "Beim alten Saatgut ohne gentechnik bleiben";
     String question4Option1Text = "Mit einer großen Supermarktkette kollaborieren.";
     String question4Option2Text = "Eine Direct-To-Consumer Online-Plattform aufbauen.";
     String question5Option1Text = "Ja, Photovoltaik-Anlage installieren.";
     String question5Option2Text = "Nein, keine Photovoltaik-Anlage installieren und auf traditionelle Energiequellen setzen.";
-    String question6Option1Text = "Diversifizierung der Produktpalette ";
+    String question6Option1Text = "Diversifizierung der Produktpalette um sich von der Konkurenz abzuheben und auf Nischenprodukte setzen";
     String question6Option2Text = "Preissenkung, um wettbewerbsfähig zu bleiben";
     String question7Option1Text = "Strikte Umsetzung und Verbesserung der Regularien";
-    String question7Option2Text = "Minimale Umsetzung der Regularien";
+    String question7Option2Text = "Minimale Umsetzung der Regularien beibehalten";
     //Platzhalter Traktor
     String question8Option1Text = "Ja, einen autonomen Traktor anschaffen.";
     String question8Option2Text = "Nein, keinen autonomen Traktor anschaffen und auf traditionelle Methoden setzen. ";
@@ -65,8 +65,8 @@ public class GameController {
     String question17Option1Text = "Ja, die Entscheidung juristisch bekämpfen.";
     String question17Option2Text = "Nein, die Entscheidung akzeptieren. ";
 
- int[] questions1Attributes1Change = {0, -40, +5, +20, +5, -3};
-    int[] questions1Attributes2Change = {+5, -20, 0, +15, 5, -10};
+ int[] questions1Attributes1Change = {0, 0, 0, 20, 0, 0};
+    int[] questions1Attributes2Change = {20, 0, 0, 0, 0, 0};
     int[] questions2Attributes1Change = {0, 20, -15, -30, -10, +10};
     int[] questions2Attributes2Change = {-20, -20, +30, +50, +15, -5};
     int[] questions3Attributes1Change = {50, 30, 20, -30, -50, -40};
@@ -101,7 +101,9 @@ public class GameController {
     int[] questions17Attributes2Change = {-20, 10, -5, -10, -10, 0};
 
 
-
+    public static Player getPlayer() {
+        return player;
+    }
 
 
     public GameController() {
@@ -148,7 +150,10 @@ public class GameController {
     }
 
     private void nextScenario() {
-        // logic to move to the next scenario
+        // TODO: Endscreens einbauen falls wert gedropt ist
+        if (player.getNachhaltigkeit() <= 0 || player.getErnte() <= 0 || player.getVermoegen() <= 0 || player.getMitarbeiterzufriedenheit() <= 0 || player.getAnsehen() <= 0) {
+            endGame();
+        }
         if (scenarioNumber >= (questions.length - 1)) {
             endGame();
         }
@@ -212,8 +217,6 @@ public class GameController {
         int risk = player.getRisiko();
         double random = Math.random() * 100;
         String []reasontoEndGame = new String[10];
-        System.out.println(risk);
-        System.out.println(random);
         if (random < risk) {
             endGame();
     }
