7d5b4d5606f88454c21e0a200c62cdfe45255b91
Vasco Pogade <118736868+VascoPogade@users.noreply.github.com>
2024-06-25 16:18:02 +0200

    added comments

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 public class App {
+    //starting point of the application
     public static void main(String[] args) {
         // Create a new GameController object
         GameController game = new GameController();
